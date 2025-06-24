448216b353d33a29c1c088bf4e9f1b2281118a50
MarioFilian <mafilian@uce.edu.ec>
2025-06-24 12:09:51 -0500

    🔥 feat: add comments for webhook testing and emoji demonstration

--- Webhook/client.js
+++ Webhook/client.js
@@ -3,3 +3,5 @@ const axios = require('axios');
 axios.post('http://localhost:3002/webhook', { data: 'test' })
   .then(res => console.log('Respuesta Webhook:', res.data))
   .catch(console.error);
+
+//esta es una prueba de webhook
